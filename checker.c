7f1f32336c9395b4aaceffcc79030406f0ea5fdc
Charlie <charlierdlp@gmail.com>
2021-06-01 21:02:00 +0200

    Fix: some parsing

--- checker.c
+++ checker.c
@@ -17,6 +17,19 @@ void    rm_lst(void *node)
     free(node);
 }
 
+void 	free_args(char **args)
+{
+	int i;
+
+	i = 0;
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
+
 int is_equal(t_stack *a)
 {
     
@@ -33,16 +46,15 @@ int    is_sorted(t_stack *a)
         lst = a->head->next;
         while (lst)
         {
-            //printf("prev: |%d|\n > lst: |%d|", *((int *)prev->content), *((int *)lst->content));
             if (prev > (int)lst->content)
             {
-                write(1, "KO\n", 2);
+                write(1, "KO\n", 3);
                 return (1);
             }
             prev = (int)lst->content;
             lst = lst->next;
         }
-        write(1, "OK\n", 2);
+        write(1, "OK\n", 3);
     }
     return (0);
 }
@@ -57,26 +69,55 @@ int check_instruction(char *line, t_stack *a, t_stack *b)
         rotate(a);
     else if (!ft_strcmp(line, "rra"))
         rev_rotate(a);
-    else if (!ft_strcmp(line, "is"))
-        is_sorted(a);
     else
         return (-1);
     return (0);
 }
 
-int parse_args(char *arg)
+int myisdigit(char c)
+{
+	printf("%c", c);
+	return (c == '-' || ft_isdigit(c));
+}
+
+int	fill_stack(char *args, t_stack *a)
+{
+	int *num;
+	t_list *node;
+
+	num = malloc(sizeof(int));
+	num[0] = ft_atoi(args);
+	node = ft_lstnew(num);
+	ft_lstadd_back(&a->head, node);
+}
+
+int parse_args(char *argv, t_stack *a)
 {
     int i;
+	int j;
+	int k;
+	char **args;
 
-    i = 0;
-        if (ft_strchr(arg, ' '))
-        {
-            if (ft_strchr(arg, '-'))
-                return (1);
-            if (!ft_isdigit(arg[i]))
-                return (1);
-        }
-        i++;
+		j = 0;
+		args = ft_split(argv, ' ');
+		while (args[j])
+		{
+			k = 0;
+			if ((args[j][k] == '-' && ft_isdigit(args[j][k + 1])) || ft_isdigit(args[j][k]))
+			{
+				fill_stack(args[j], a);
+				i++;
+				k++;
+			}
+			else
+			{
+				printf("Error\n");
+				free_args(args);
+				return (1);
+			}
+			free_args(args);
+			j++;
+		}
     return (0);
 }
 
@@ -97,28 +138,34 @@ int main(int argc, char **argv)
     b.head = NULL;
     if (argc > 1)
     {
-        while (argv[i]) // lo de las comillas de alvrodri
-        {
-            if (parse_args(argv[i]))
+		while (argv[i])
+		{
+            if (parse_args(argv[i], &a))
                 return (1);
+			/*
             num = malloc(sizeof(int));
             num[0] = ft_atoi(argv[i]);
             node = ft_lstnew(num);
             ft_lstadd_back(&a.head, node);
+			*/
             i++;
-        }
+		}
     }
     while (get_next_line(0, &line) > 0 && r)
     {
        r = check_instruction(line, &a, &b);
        free(line);
     }
+	is_sorted(&a);
+    /*
    t_list *lst = a.head;
     while (lst)
     {
         printf("%d\n", *((int *)lst->content));
         lst = lst->next;
     }
+    */
     ft_lstclear(&a.head, &rm_lst);
+//	system("leaks checker");
     return (0);
 }
